a8c76e4bd2d023ff511b92199f91a818517b1752
rill <saharilarshad@gmail.com>
2022-05-12 12:04:06 +0800

    jsonserver

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import {useContext} from "react";
+import DataContext from "./context/DataContext";
+import "./footer.css"
 
 export default function Footer() {
   const today = new Date();
+  const { posts} = useContext(DataContext)
   return (
     <footer>
+      <p>{posts.length} List {posts.length === 1 ? "item" : "items"}</p>
       <p>Copyright &copy; {today.getFullYear()}</p>
     </footer>
   );
